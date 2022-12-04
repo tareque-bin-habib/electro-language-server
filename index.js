790ffa1ba00b325bb7ae4bd64845176e38da240b
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-04 20:58:41 +0600

    single api added

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 
-
-const products = require('./data/products.json')
+// const number = [1, 5, 3, 4, 8,]
+const products = require('./data/products.json');
 
 
 app.get('/', (req, res) => {
@@ -18,6 +18,14 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
     res.send(products)
 })
+
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedDetails = products.find(p => p.id === id)
+    res.send(selectedDetails)
+})
+
+
 
 app.listen(port, () => {
     console.log('electro learning port running on', port);
